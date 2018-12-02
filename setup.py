2ed995531efab8328b4fcf4571fbc5cf1406e3ac
Lev <subveles@gmail.com>
2018-12-02 16:59:23 +0300

    fixed setup

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(name='Python3-Youtrack-api',
       author_email='grander.stark@yandex.ru',
       license='Apache 2.0',
       zip_safe=False,
+      packages=[
+          'python3_youtrack_api'
+      ],
+      package_dir={'': 'package'},
       install_requires=[
           'requests',
           'marshmallow',
